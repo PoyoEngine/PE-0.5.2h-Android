6cc0aa8ddf676f82be2d1ed12f124b47e1000fbc
RecoloredPoyo4 <82355015+RecoloredPoyo4@users.noreply.github.com>
2022-04-07 14:01:11 -0400

    optimization mode 1/?

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -37,6 +37,13 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 		rpcTitle = 'Graphics Settings Menu'; //for Discord Rich Presence
 
 		//I'd suggest using "Low Quality" as an example for making your own option since it is the simplest here
+		var option:Option = new Option('Optimized Mode', //Name
+			'If checked, preventd BF, GF, Dad, BG, Title Screen and Main Menu Assets\nfrom being added.', //Description
+			'lowQuality', //Save data variable name
+			'bool', //Variable type
+			false); //Default value
+		addOption(option);
+		
 		var option:Option = new Option('Low Quality', //Name
 			'If checked, disables some background details,\ndecreases loading times and improves performance.', //Description
 			'lowQuality', //Save data variable name
